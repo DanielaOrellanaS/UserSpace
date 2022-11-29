8d49f8054970e1d09211f1cb446ad92d3de859c7
Daniela Estefania Orellana Soriano <63482841+DanielaOrellanaS@users.noreply.github.com>
2022-11-28 19:50:59 -0500

    Project Creation

---
+++ ArqSoftwareExamen/espacio/src/main/java/com/user/espacio/EspacioApplication.java
@@ -0,0 +1,13 @@
+package com.user.espacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacioApplication.class, args);
+	}
+
+}
---
+++ ArqSoftwareExamen/espacio/src/test/java/com/user/espacio/EspacioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.espacio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
